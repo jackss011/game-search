40de0a07faaaa3f6bbd4c8b57fe1e180c80d5111
Giacomo Agnetti <jackss.code@gmail.com>
2021-08-18 21:03:50 +0200

    Using prettier

--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const api = require("./api");
+const express = require('express')
+const api = require('./api')
 
-const app = express();
-const PORT = 1290;
+const app = express()
+const PORT = 1290
 
-app.get("/wishlist/:id", async (req, res) => {
+app.get('/wishlist/:id', async (req, res) => {
   try {
-    const wishlistArray = await api.steamUserWishList(req.params.id);
-    res.json({ items: wishlistArray });
+    const wishlistArray = await api.steamUserWishList(req.params.id)
+    res.json({ items: wishlistArray })
   } catch (e) {
-    res.sendStatus(500);
-    console.error(e);
+    res.sendStatus(500)
+    console.error(e)
   }
-});
+})
 
-app.listen(PORT, () => console.log("Server started..."));
+app.listen(PORT, () => console.log('Server started...'))
 
 // steamDbPriceHistory(788100).then((r) => console.log(r));
 // steamUserWishList("jackss14").then((r) => console.log(r.slice(0, 10)));
